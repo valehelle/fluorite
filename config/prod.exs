4b1408493593e95ddfdbb2387e936845e7a6628e
Hazmi Irfan <hazmi.irfan.zainal@rhbgroup.com>
2020-12-28 16:32:01 +0800

    change prod domain

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :fluorite, FluoriteWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "infinite-bastion-21639.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "www.getfluorite.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
